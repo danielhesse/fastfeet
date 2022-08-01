bcc2552dc99ee66b59abfb26d3dc1969b00bb45d
Daniel Hessel <daniel@sharbe.com.br>
2022-08-01 06:31:00 -0300

    refactor: refactor project structure

--- start/routes/sessions.routes.ts
+++
@@ -1,3 +0,0 @@
-import Route from '@ioc:Adonis/Core/Route'
-
-Route.post('/sessions', 'SessionsController.create')
--- start/routes/user.routes.ts
+++
@@ -1,4 +0,0 @@
-import Route from '@ioc:Adonis/Core/Route'
-
-Route.post('/users', 'UsersController.create')
-Route.put('/users', 'UsersController.update').middleware('auth')
